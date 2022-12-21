f02bea4e393c17f2e22a33af059172b3b728f1d4
Mark Tellez <mark@devmentor.live>
2022-12-21 08:34:34 -0600

    npm ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DepthTable } from "./components/depth-table";
+import { InstaBid } from "./components/insta-bid";
+import { InstaForm } from "./components/insta-form";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { DepthTable, InstaBid, InstaForm };
