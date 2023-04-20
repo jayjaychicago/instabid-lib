a5f705f1ea300b5373e950e45e80da4720eb8000
jayjaychicago <julienpmjacquet@gmail.com>
2023-04-20 17:00:55 -0400

    bug

--- src/components/Instabid.jsx
+++ src/components/Instabid.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { DepthTable } from "./DepthTable.jsx";
 import { InstaForm } from "./InstaForm.jsx";
 
-export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy }) => {
+export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy, authUrl }) => {
   return (
     <div>
       <DepthTable
         exchange={exchange}
         product={product}
         user={user}></DepthTable>
-      <InstaForm exchange={exchange} product={product} user={user} devModeApiKey={devModeApiKey} apiProxy={apiProxy}></InstaForm>
+      <InstaForm exchange={exchange} product={product} user={user} devModeApiKey={devModeApiKey} apiProxy={apiProxy} authUrl={authUrl}></InstaForm>
     </div>
   );
 };
