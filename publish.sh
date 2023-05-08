6bbaa1b2c1a602e583d0fbf649b6814c60b04c8b
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-07 21:32:36 -0400

    bug fix

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Read the current version from package.json
-current_version=$(grep -oP '(?<="version": ")[^"]+' package.json)
+current_version=$(jq -r '.version' package.json)
 
 # Increment the version
 new_version=$(echo "$current_version" | awk -F. -v OFS=. '{$NF++;print}')
 
 # Update the version in package.json
-sed -i "s/$current_version/$new_version/g" package.json
+jq ".version = \"$new_version\"" package.json > package_temp.json && mv package_temp.json package.json
 
 # Build the project
 yarn build
