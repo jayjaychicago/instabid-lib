0cbe5f60fa2b65440bfd3d3abd5947c2f22b2c48
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-07 22:25:04 -0400

    bug fix

--- src/components/Instabid.jsx
+++ src/components/Instabid.jsx
@@ -5,13 +5,26 @@ import { OrderTable } from "./OrderTable.jsx";
 
 export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy, authUrl }) => {
   return (
-    <div>
-      <DepthTable
+<div className="d-flex flex-column w-100 align-items-center">
+            <Tabs defaultActiveKey="makeBid"
+              id="uncontrolled-tab"
+              className="justify-content-center">
+              <Tab eventKey="makeBid" title="Make a Bid">
+              <DepthTable
         exchange={exchange}
         product={product}
         user={user}></DepthTable>
       <InstaForm exchange={exchange} product={product} user={user} devModeApiKey={devModeApiKey} apiProxy={apiProxy} authUrl={authUrl}></InstaForm>
+            </Tab>
+            <Tab eventKey="orders" title="Orders">
+            <OrderTable
+        exchange={exchange}
+        product={product}
+        user={user}></OrderTable>
+            </Tab>
+            </Tabs>
+</div>
 
-    </div>
+
   );
 };
