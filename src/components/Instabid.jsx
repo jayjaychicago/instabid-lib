fb41adfc461b2647ac7a2b03c0506297eaa2ccec
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-07 23:06:18 -0400

    bug fix

--- src/components/Instabid.jsx
+++ src/components/Instabid.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { DepthTable } from "./DepthTable.jsx";
 import { InstaForm } from "./InstaForm.jsx";
+import { OrderTable } from "./OrderTable.jsx";
+import {Tabs, Tab} from 'react-bootstrap';
 
-export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy, authUrl }) => {
+export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy, authUrl, mode }) => {
+  if (mode == "withOrders") {
+    return (
+      <div className="d-flex flex-column w-100 align-items-center">
+                  <Tabs defaultActiveKey="makeBid"
+                    id="uncontrolled-tab"
+                    className="justify-content-center">
+                    <Tab eventKey="makeBid" title="Make a Bid">
+                    <DepthTable
+              exchange={exchange}
+              product={product}
+              user={user}></DepthTable>
+            <InstaForm exchange={exchange} product={product} user={user} devModeApiKey={devModeApiKey} apiProxy={apiProxy} authUrl={authUrl}></InstaForm>
+                  </Tab>
+                  <Tab eventKey="orders" title="Orders">
+                  <OrderTable
+              exchange={exchange}
+              product={product}
+              user={user}></OrderTable>
+                  </Tab>
+                  </Tabs>
+      </div>
+        );
+  } else {
   return (  <div>
     <DepthTable
       exchange={exchange}
@@ -10,7 +35,6 @@ export const Instabid = ({ exchange, product, user, devModeApiKey, apiProxy, aut
       user={user}></DepthTable>
     <InstaForm exchange={exchange} product={product} user={user} devModeApiKey={devModeApiKey} apiProxy={apiProxy} authUrl={authUrl}></InstaForm>
   </div>
-
-
   );
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { DepthTable } from "./components/DepthTable.jsx";
 import { Instabid } from "./components/Instabid.jsx";
-import { InstabidWithOrders } from "./components/InstabidWithOrders.jsx";
 import { InstaForm } from "./components/InstaForm.jsx";
 import { OrderTable } from "./components/OrderTable.jsx";
 
 
 export default Instabid;
 
-export { InstabidWithOrders, DepthTable, InstaForm, OrderTable };
+export { DepthTable, InstaForm, OrderTable };
