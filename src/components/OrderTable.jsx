947e808e9c5e58b18f7ec8bc74eab1044835366f
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-07 21:05:45 -0400

    fixed a bunch of stuff

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Pusher from "pusher-js";
 import { DataGrid } from "@mui/x-data-grid";
 
-import { updateSortedOrders } from "../util";
-
 const EVENT_NAME = "ORDERUPDATE";
 
 export function OrderTable({ exchange, product, user }) {
@@ -66,28 +64,8 @@ export function OrderTable({ exchange, product, user }) {
     return time.toLocaleTimeString();
   }
 
-  function cancelOrderBtn(cell, row) {
-    if (row.qtyLeft !== 0 && row.user === "julien@instabid.io") {
-      return (
-        <button className="btn btn-danger btn-sm" onClick={() => cancelOrder(row.orderNumber)}>
-          Cancel
-        </button>
-      );
-    }
-    return "";
-  }
-
-  function cancelOrder(orderNumber) {
-    console.log("Cancel order:", orderNumber);
-    // Implement your cancel order logic here
-  }
-
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
   const columns = [
-    { field: "orderNumber", headerName: "Order Number", width: 150, sortable: true },
+    { field: "orderNumber", headerName: "#", width: 150, sortable: true },
     { field: "exchange", headerName: "Exchange", width: 150, sortable: true, filterable: true },
     { field: "product", headerName: "Product", width: 150, sortable: true, filterable: true },
     { field: "side", headerName: "Side", width: 120, sortable: true, filterable: true },
@@ -124,29 +102,38 @@ export function OrderTable({ exchange, product, user }) {
   return (
     <div className="h-100 d-flex align-items-center justify-content-center">
       <div id="orders" style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={orders}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        disableSelectionOnClick
-        sortingOrder={['desc', 'asc']}
-        sortModel={[
+        <DataGrid
+          rows={orders}
+          columns={columns}
+          pageSize={50}
+          rowsPerPageOptions={[50]}
+          rowHeight={40}
+          disableSelectionOnClick
+          sortingOrder={['desc', 'asc']}
+          sortModel={[
             {
-            field: 'date',
-            sort: 'desc',
-            },
-            {
-            field: 'time',
-            sort: 'desc',
-            },
-        ]}
-        getRowId={(row) =>
+              field: 'orderNumber',
+              sort: 'desc',
+            }
+          ]}
+          getRowId={(row) =>
             `${row.exchange}-${row.product}-${row.side}-${row.timestamp}-${row.orderNumber}`
-        }
+          }
+          components={{
+            columnHeader: {
+              style: {
+                fontWeight: 'bold',
+              },
+            },
+            pagination: {
+              style: {
+                display: 'flex',
+                alignItems: 'center',
+              },
+            },
+          }}
         />
-
       </div>
     </div>
   );
-}
+        }
