bdeaf904425fd0324d8ab443786414a552fc94a3
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-16 14:28:02 -0400

    bug fix

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -155,11 +155,9 @@
                         if (!res.ok) {
                             throw new Error(`HTTP error! status: ${res.status}`);
                         }
-                        else {
-                            console.log('ORDER TABLE GET API CALL returned ', res);
-                        }
-        
-                        handleData(await res.json());
+                        let ress = await res.json();
+                        console.log('ORDER TABLE GET API CALL returned ', ress.result);
+                        handleData(ress);
                     } catch (error) {
                         console.error('Fetch error:', error);
                     }
