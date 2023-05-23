69a1d75a45079144996b71a8192f744374bc9545
jayjaychicago <julienpmjacquet@gmail.com>
2023-05-22 21:31:29 -0400

    bug fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import { DepthTable } from "./components/DepthTable.jsx";
 import { Instabid } from "./components/Instabid.jsx";
 import { InstaForm } from "./components/InstaForm.jsx";
 import { OrderTable } from "./components/OrderTable.jsx";
-import {Test} from "./components/Test.jsx"
-
+import { FillTable } from "./components/FillTable.jsx";
 
 export default Instabid;
 
-export { DepthTable, InstaForm, OrderTable, Test }; 
+export { DepthTable, InstaForm, OrderTable, FillTable }; 
